99e7126a09d3b2077755e76818fdebc32113d1ad
Don Kackman <dkackman@gmail.com>
2013-12-08 13:56:22 -0600

    initial merge of locations code

---
+++ src/Bing.RestClient.Tests/Locations/AddressTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Bing.Locations;
+
+namespace GeoCoderTests
+{
+    [TestClass]
+    public class AddressTests
+    {
+        private BingLocationsRestClient _service;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _service = new BingLocationsRestClient(APIKEY.Key, "Portable Bing GeoCoder unit tests");
+        }
+
+        [TestMethod]
+        public async Task GetAddress()
+        {
+            var address = await _service.GetAddress(44.9108238220215, -93.1702041625977);
+
+            Assert.IsNotNull(address);
+        }
+
+        [TestMethod]
+        public async Task RoundtripPostalCode()
+        {
+            var coord = await _service.GetCoordinate(new Address() { postalCode = "55116", countryRegion = "US" });
+            var address = await _service.GetAddress(coord.Item1, coord.Item2);
+
+            Assert.AreEqual(address.postalCode, "55116");
+        }
+
+        [TestMethod]
+        public async Task ParseAnAddress()
+        {
+            var address = await _service.ParseAddress("One Microsoft Way, Redmond, WA 98052");
+
+            Assert.AreEqual(address.addressLine, "1 Microsoft Way");
+            Assert.AreEqual(address.locality, "Redmond");
+            Assert.AreEqual(address.adminDistrict, "WA");
+            Assert.AreEqual(address.postalCode, "98052");
+            Assert.AreEqual(address.countryRegion, "United States");
+        }
+
+        [TestMethod]
+        public async Task ParseACanadianAddress()
+        {
+            //var coord = new Tuple<double, double>(62.832908630371094, -95.913322448730469);
+            //var service = new GeoCoder(APIKEY.Key, "Portable Bing GeoCoder unit tests", "en-CA", new UserContext(coord));
+
+            var address = await _service.ParseAddress("1950 Meadowvale Blvd., Mississauga, ON L5N 8L9");
+
+            Assert.AreEqual(address.addressLine, "1950 Meadowvale Blvd");
+            Assert.AreEqual(address.locality, "Mississauga");
+            Assert.AreEqual(address.adminDistrict, "ON");
+            Assert.AreEqual(address.postalCode, "L5N 8L9");
+            Assert.AreEqual(address.countryRegion, "Canada");
+        }
+    }
+}
---
+++ src/Bing.RestClient/Locations/ResponseExtensions.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Bing.Locations
+{
+    /// <summary>
+    /// Helper methods for spelunking the GeoResultSet and its child collections
+    /// </summary>
+    public static class GeoResultExtensions
+    {
+        public static IEnumerable<Address> GetAddresses(this Response result)
+        {
+            return from rs in result.resourceSets
+                   from r in rs.resources
+                   select r.address;
+        }
+
+        public static IEnumerable<Tuple<double, double>> GetCoordinates(this Response result)
+        {
+            return from rs in result.resourceSets
+                   from r in rs.resources
+                   let pts = r.point.coordinates
+                   select new Tuple<double, double>(pts[0], pts[1]);
+        }
+
+        public static Address GetFirstAddress(this Response result)
+        {
+            return result.GetAddresses().FirstOrDefault();
+        }
+
+        public static Tuple<double, double> GetFirstCoordinate(this Response result)
+        {
+            return result.GetCoordinates().FirstOrDefault() ?? new Tuple<double, double>(0, 0);
+        }
+
+        public static string GetFirstAddressProperty(this Response result, Func<Address, string> selector)
+        {
+            Debug.Assert(selector != null);
+
+            var address = result.GetFirstAddress();
+            if (address != null)
+                return selector(address);
+
+            return "";
+        }
+
+        public static string GetFirstFormattedAddress(this Response result)
+        {
+            return result.GetFirstAddressProperty(a => a.formattedAddress);
+        }
+
+        public static string GetFirstAddressPart(this Response result, string part)
+        {
+            return result.GetFirstAddressProperty(GetAddressPartSelector(part));
+        }
+
+        private static Func<Address, string> GetAddressPartSelector(string part)
+        {
+            if (part.Equals("address", StringComparison.OrdinalIgnoreCase))
+                return a => a.addressLine;
+
+            if (part.Equals("Neighborhood", StringComparison.OrdinalIgnoreCase))
+                return a => a.neighborhood;
+
+            if (part.Equals("postcode1", StringComparison.OrdinalIgnoreCase))
+                return a => a.postalCode;
+
+            if (part.Equals("PopulatedPlace", StringComparison.OrdinalIgnoreCase))
+                return a => a.locality;
+
+            if (part.Equals("AdminDivision1", StringComparison.OrdinalIgnoreCase))
+                return a => a.adminDistrict;
+
+            if (part.Equals("AdminDivision2", StringComparison.OrdinalIgnoreCase))
+                return a => a.adminDistrict2;
+
+            if (part.Equals("CountryRegion", StringComparison.OrdinalIgnoreCase))
+                return a => a.countryRegion;
+
+            Debug.Assert(false);
+            return a => "";
+        }
+    }
+}
