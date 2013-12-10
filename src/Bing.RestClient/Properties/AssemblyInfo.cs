a34ef31030b118e15caeb7d0642f2e0dfd93ea39
advancedrei <robert@advancedrei.com>
2013-12-09 23:36:03 -0500

    - Increment the version number.

--- src/Bing.RestClient/Properties/AssemblyInfo.cs
+++ src/Bing.RestClient/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Bing.RestClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Portable Class Library for accessing Bing REST APIs.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Bing.RestClient")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.5.0")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
